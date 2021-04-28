9fcfa3c0e25881e66f19cfe244d97d086539e064
pradeep5299 <tpradeep5299@gmail.com>
2021-04-28 14:25:40 +0530

    Done EMS

---
+++ EMS/src/com/mphasis/Exception/UserNotFoundException.java
@@ -0,0 +1,17 @@
+
+
+package com.mphasis.Exception;
+
+public class UserNotFoundException {
+
+	public UserNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public String toStringg() {
+		return "EmployeeNotFoundException []";
+	}
+	
+}
---
+++ EMS/src/com/mphasis/model/Employee.java
@@ -0,0 +1,44 @@
+package com.mphasis.model;
+
+public class Employee {
+
+	private int eid;
+	private String ename;
+	private Salary salary;
+
+	public Employee() {
+		System.out.println("From Emp constr...");
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getEname() {
+		return ename;
+	}	
+
+	public Salary getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Salary salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", salary=" + salary + "]";
+	}
+
+	
+
+}
